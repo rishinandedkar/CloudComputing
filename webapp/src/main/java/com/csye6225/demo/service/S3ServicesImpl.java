dc53e6b98a25b9900ba6fe69b8fcb86946868946
gauri <sarpotdar.g@northeastern.edu>
2018-10-26 17:28:00 -0400

    tryingstill

--- webapp/src/main/java/com/csye6225/demo/service/S3ServicesImpl.java
+++ webapp/src/main/java/com/csye6225/demo/service/S3ServicesImpl.java
@@ -27,7 +27,7 @@ public class S3ServicesImpl implements S3Services{
     private AmazonS3 s3client;
 
 
-    private String bucketName = "csye6225-fall2018-sarpotdarg.me";
+    private String bucketName = "code-deploy.csye6225-fall2018-sarpotdarg.me.csye6225.com";
 
 
     @Override
@@ -47,7 +47,7 @@ public class S3ServicesImpl implements S3Services{
     @Override
     public void deleteReceiptFromS3(String key) {
         try{
-          s3client.deleteObject("csye6225-fall2018-sarpotdarg.me", key);
+          s3client.deleteObject("code-deploy.csye6225-fall2018-sarpotdarg.me.csye6225.com", key);
        //s3client.deleteObject(new DeleteObjectRequest("rishicsye6225", key));
         }
         catch(Exception ex){
